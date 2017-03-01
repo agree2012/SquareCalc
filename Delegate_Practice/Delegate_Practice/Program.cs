449aa930f20b24e8ea02f0722fa40775c14dfb3c
Ilya <agree2012@yandex.ru>
2017-03-01 14:06:52 +0400

    +++Delegate try+++

---
+++ Delegate_Practice/Delegate_Practice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate_Practice
+{
+    class Program
+    {
+        
+        delegate void GetMessage();
+        static void Main(string[] args) 
+        {
+            GetMessage message = delegate
+            {
+            Console.WriteLine("анонимный делегат");
+            };
+            message();
+            Console.Read();
+         }
+     }
+    
+}
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -10,19 +10,16 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
-            
         }
         
         public void button_activate_Click_1(object sender, EventArgs e)
-        {
-            
-            pictureBox1.Load(Application.StartupPath + @"\cbmp.bmp");
+        {   
+            pictureBox1.Load(Application.StartupPath + @"\cbmp2.bmp");
             Bitmap bmp = new Bitmap(pictureBox1.Image.Width+1, pictureBox1.Image.Height+1);
             pictureBox1.DrawToBitmap(bmp, new Rectangle(0, 0, pictureBox1.Image.Width, pictureBox1.Image.Height));
             pictureBox1.Image = bmp;
             var res = Processor.Calc(bmp);
             MessageBox.Show($"Количество квадратов: {res.Square} Количество прямоугольников: {res.Rectangle} ", @"Количество квадратов", MessageBoxButtons.OK);                    
-                                
         }   
     }          
 }
